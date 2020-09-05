c59e6cfca4d9c58dd34f41a2f408b1a3c48664d7
Bruno Rafael <brunorafaelap@gmail.com>
2020-09-05 05:34:12 +0200

    Adjust: removed create element with document to string tags html for innerHTML

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 function changeTextColorSlidingTheDiv(text) {
-  const slid_section = document.createElement("section");
-  const wrap_div = document.createElement("div");
+  var html = "";
+  var content = document.querySelector(".content");
 
-  // Set attributes
-  wrap_div.setAttribute("class", "wrapper");
+  html += `
+    <section>
+      <span class="sliding">
+        ${createContentAroundText()}
+      </span>
+      <div class="wrapper">
+        <span class="sliding">
+          ${createContentAroundText()}
+        </span>
+      </div>
+    </section>
+  `;
 
-  wrap_div.appendChild(createContentAroundText());
-  slid_section.appendChild(createContentAroundText());
-
-  slid_section.appendChild(wrap_div);
-  document.body.appendChild(slid_section);
+  content.innerHTML = html;
 
   function createContentAroundText() {
-    const slid_span = document.createElement("span");
+    var span = "";
+    var arrayText = text.trim().split(" ");
 
-    // Set attributes
-    slid_span.setAttribute("class", "sliding");
+    arrayText.forEach((text) => (span += `<span> ${text}</span>`));
 
-    const arrayText = text.trim().split(" ");
-
-    arrayText.forEach((text) => {
-      const span = document.createElement("span");
-      const span_text = document.createTextNode(` ${text}`);
-
-      span.appendChild(span_text);
-      slid_span.appendChild(span);
-    });
-
-    return slid_span;
+    return span;
   }
 }
